207c5eae78be6add431bc68f408bb17642ff9d46
Anton <anton.shvetsov@mail.ru>
2020-02-06 00:21:57 +0300

    update function myMap,sum,reduce

--- map.js
+++ map.js
@@ -1,10 +1,13 @@
-//test function for MyFunction
+//test data
+var array1 = [1,2,3,4,5];
+var array2 = ['1','2','3','4','5'];
+var array3 = ['']
+//Reduce
 function sum(el){
     return el*el;
 };
 
-//Function MyMap
-function myMap(array,Fn){
+function myReduce(array,Fn){
     let result;
     
     if(array[0] instanceof String) result = '';
@@ -15,9 +18,34 @@ function myMap(array,Fn){
 
     return result;
 };
+//alert(myReduce(array1,sum));
+//alert(myReduce(array2,sum));
 
-var array1 = [1,2,3,4,5];
-var array2 = ['1','2','3','4','5'];
+//myMap
+function myMap(arr,Fn){
+    let result = [];
+    
+    arr.forEach(element =>{
+        result.push(Fn(element));
+    });
+    
+    return result;
+}  
 
 alert(myMap(array1,sum));
 alert(myMap(array2,sum));
+
+function myQuicksort(arr){
+    
+    var array = arr.split;
+    var temp, index = 0;
+
+    while()
+        if(element[index] > element[index+1]){
+            temp = element;
+            element = element+1;
+            element+1=temp;
+
+        }
+    
+};
